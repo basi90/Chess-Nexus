406c24035d24a7dbb8fd059e5d0ae411f2940e67
Andrea Basili <andrea.basili@live.it>
2024-03-14 17:11:19 +0100

    defined the logic of the moves for the chess pieces, pawn and king are incomplete

--- app/models/pieces/queen.rb
+++ app/models/pieces/queen.rb
@@ -5,6 +5,22 @@ class Queen < Piece
   end
 
   def to_s
-    color == :white ? "♕" : "♛"
+    color == :white ? "♛"  : "♕"
+  end
+
+  def move_to(new_position)
+    self.current_position = new_position
+  end
+
+  def valid_moves(current_position, board_state)
+    valid_moves = []
+
+    bishop = Bishop.new(color)
+    rook = Rook.new(color)
+
+    valid_moves += bishop.valid_moves(current_position, board_state)
+    valid_moves += rook.valid_moves(current_position, board_state)
+
+    return valid_moves
   end
 end
--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -1,5 +1,4 @@
 class Rook < Piece
-  include Tableless
 
   def initialize(color)
     super(color)
@@ -7,6 +6,35 @@ class Rook < Piece
   end
 
   def to_s
-    color == :white ? "♖" : "♜"
+    color == :white ?  "♜" : "♖"
+  end
+
+  def move_to(new_position)
+    self.current_position = new_position
+    @moved = true
+  end
+
+  def valid_moves(current_position, board_state)
+    valid_moves = []
+
+    cardinals = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    cardinals.each do |cardinal|
+      x, y = current_position
+      while true
+        x += cardinal[0]
+        y += cardinal[1]
+        break unless x.between?(0, 7) && y.between?(0, 7)
+
+        if board_state[x][y].nil?
+          valid_moves << [x, y]
+        elsif board_state[x][y].color != color
+          valid_moves << [x, y]
+          break
+        else
+          break
+        end
+      end
+    end
+    valid_moves
   end
 end
