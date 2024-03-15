30cd36ecf8be949db621b4d3a1ad03d7bbb3e139
Andrea Basili <andrea.basili@live.it>
2024-03-15 13:04:39 +0100

    changed the models placement

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,13 +1,3 @@
-require_relative "pieces/piece"
-require_relative "pieces/rook"
-require_relative "pieces/knight"
-require_relative "pieces/bishop"
-require_relative "pieces/queen"
-require_relative "pieces/king"
-require_relative "pieces/pawn"
-
-
-
 class Board
   attr_accessor :board_state, :current_position
 
